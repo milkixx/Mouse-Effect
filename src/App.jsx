2f400c22e84733e68bd7a2c254a1a12a2b1d58c4
Max Milkin <48333410+milkixx@users.noreply.github.com>
2024-10-02 22:52:44 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MouseEffect from './MouseEffect';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>add this mouse effect to your website</h1>
+      <MouseEffect />
+    </div>
+  );
+}
+
+export default App;
